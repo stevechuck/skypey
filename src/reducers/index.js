6f49bfa1445a1ee5c505b05b955c8dbe9b708952
iamchuckss <s2011297@gmail.com>
2019-06-30 16:23:31 -0500

    refactor reducers
    - refactor index.js to combine multiple reducers
    - make contactsReducer handle initial state instead of store/index.js
    - add contactReducer to handle contacts
    - add userReducer to handle users

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,7 @@
-export default (state, action) => {
-    return state;
-};
+import { combineReducers } from "redux";
+import contacts from "./contacts";
+import user from "./user";
+export default combineReducers({
+    user,
+    contacts,
+});
