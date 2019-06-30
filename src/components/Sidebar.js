a0fdc597b35b1f7f3b56ac11ac81e809dbe42b1a
iamchuckss <s2011297@gmail.com>
2019-06-30 15:33:55 -0500

    refactor project structure

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,6 @@
-import React from "react";
-import User from "./User"; 
+import React from 'react';
 import "./Sidebar.css";
+import User from "../containers/User";
 
 const Sidebar = ({ contacts }) => {
   return (
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import Sidebar from './Sidebar';
-import Main from './Main';
+import Main from "../components/Main"
+import Sidebar from "../components/Sidebar"
 import './App.css';
-import store from './store';
+import store from "../store"
 import _ from "lodash";
 
-function App() {
-
+const App = () => {
   const { contacts } = store.getState();
-
   return (
     <div className="App">
       <Sidebar contacts={_.values(contacts)} />
       <Main />
     </div>
   );
-}
+};
+
 
 export default App;
