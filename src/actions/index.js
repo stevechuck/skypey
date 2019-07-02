a2d923d7d4a5076866601bd1da14e709c08324f0
iamchuckss <s2011297@gmail.com>
2019-07-02 18:25:22 -0500

    setup Typing Input Form to process user input
    - add new action sendMessage
    - update reducer messages to handle new action
    - update MessageInput container to handle user input

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import {
   SET_ACTIVE_USER_ID,
-  SET_TYPING_VALUE
+  SET_TYPING_VALUE,
+  SEND_MESSAGE,
 } from "../constants/action-types";
 
 export const setTypingValue = value => ({
@@ -12,3 +13,11 @@ export const setActiveUserId = id => ({
   type: SET_ACTIVE_USER_ID,
   payload: id
 });
+
+export const sendMessage = (message, userId) => ({
+  type: SEND_MESSAGE,
+  payload: {
+      message,
+      userId
+  }
+})
